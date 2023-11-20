f33eedb509ff056b4b315e84065292a1b76c9980
Xeriou <fly52590@gmail.com>
2023-11-20 15:35:43 +0800

    經過修正後，可以使用 @SpringBootApplication 跑了

--- plugins/PluginM/src/main/java/com/xeriou/plugins/pluginm/PluginMApplication.java
+++ plugins/PluginM/src/main/java/com/xeriou/plugins/pluginm/PluginMApplication.java
@@ -2,9 +2,12 @@ package com.xeriou.plugins.pluginm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.xeriou.plugins.pluginm"})
+@SpringBootApplication
+@EnableConfigurationProperties(WebProperties.class)
 public class PluginMApplication {
     public static void main(String[] args) {
         SpringApplication.run(PluginMApplication.class, args);
--- plugins/PluginX/src/main/java/com/xeriou/plugins/pluginx/PluginXApplication.java
+++ plugins/PluginX/src/main/java/com/xeriou/plugins/pluginx/PluginXApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
-//@SpringBootApplication
-//@SpringBootConfiguration
-//@Import({DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@EnableConfigurationProperties(WebProperties.class)
 public class PluginXApplication {
     public static void main(String[] args) {
         SpringApplication.run(PluginXApplication.class, args);
